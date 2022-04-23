68ccb6c906527a8337b776bb6cb45e49c1ce90f7
Nolo-98 <twisterfast@gmail.com>
2022-04-23 12:06:33 -0500

    Primer Router

---
+++ src/components/dc/DcScreen.js
@@ -0,0 +1,10 @@
+
+
+export const DcScreen = () => {
+    return (
+      <div>
+          <h1>DcScreen</h1>
+      </div>
+    )
+  }
+  
---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,10 @@
+
+
+export const SerchScreen = () => {
+    return (
+      <div>
+          <h1>SearchScrren</h1>
+      </div>
+    )
+  }
+  
