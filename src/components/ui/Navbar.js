a45e35d76298ff573ce16e4bf9bd7d6e25cebd54
Nolo-98 <twisterfast@gmail.com>
2022-04-23 15:35:52 -0500

    Search- Component

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -39,6 +39,14 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink 
+                        activeClassName={ ({ isActive }) => 'nav-item nav-link' +  (isActive ? 'active': '') } 
+                        className="nav-item nav-link" 
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
