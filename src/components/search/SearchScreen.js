5b49389aaa50ecedbc3b020e4afce8b9a81b3edc
Nolo-98 <twisterfast@gmail.com>
2022-04-23 16:12:08 -0500

    Aplicar filtro de Héroes - QueryString

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,3 +1,6 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string'
+
 import { useForm } from '../../hooks/useForm'
 import { getHeroesByName } from '../../selectors/getHeroesByName';
 import { HeroCard } from '../hero/HeroCard';
@@ -5,16 +8,21 @@ import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
 
-  const [ formValues, handleInputChange] = useForm({
-    searchText: '',
-  })
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = ''} = queryString.parse(location.search);
+
+  const [ formValues, handleInputChange ] = useForm({
+    searchText: q,
+  });
 
   const { searchText }  = formValues;
-  const heroesFileted = getHeroesByName('ALGO POR AQUI')
+  const heroesFileted = getHeroesByName(q);
 
   const handleSearch = (e) => {
-    e.preventDefautl();
-    console.log(searchText);
+    e.preventDefault();
+    navigate(`?q=${ searchText }`)
   }
 
     return (
--- src/selectors/getHeroesByName.js
+++ src/selectors/getHeroesByName.js
@@ -2,10 +2,11 @@ import { heroes } from '../data/heroes';
 
 export const getHeroesByName = (name = '') => {
 
-    //name = name.toLowerCase();
+    if ( name.length === 0 ) {
+        return [];
+    }
 
-    //return heroes.filter(hero => hero.name.toLowerCase().includes(name));
-
-    return [...heroes];
+    name = name.toLowerCase();
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
 
 }
